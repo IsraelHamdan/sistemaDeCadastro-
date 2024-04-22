caf1ba1b3b48c4850c4f1e8a6a90072565a8e473
Israel Hamdan <IsraelSantos_20K@outlook.com>
2024-04-22 20:47:36 -0300

    metodo buscar pelo id funcionando

--- src/sistemadecadastro/Menu.java
+++ src/sistemadecadastro/Menu.java
@@ -13,14 +13,19 @@ public class Menu {
 
     public static void Create() throws IOException {
         System.out.println("F - Cadastrar pessoa física | J - Cadastrar11 pessoa jurídica");
-        CadastroPessoaFisica.createPf();
+        type = sc.nextLine();
+        if (type.equalsIgnoreCase("F")) {
+            CadastroPessoaFisica.createPf();
+        } else if (type.equalsIgnoreCase("J")) {
+            System.out.println();
+        }
 
 
     }
     public static void Alterar() throws IOException {
         System.out.println("F - Alterar pessoa física | J - Alterar pessoa jurídica");
         type = sc.nextLine();
-        if (type.equals("F")) {
+        if (type.equalsIgnoreCase("F")) {
             PessoaFisicaRepo.alterar();
         } else if (type.equals("J")) {
             System.out.println("Chamar pessoa jurídica.");
@@ -29,12 +34,12 @@ public class Menu {
     public static void Excluir() {
         System.out.println("F - Excluir pessoa física | J - Excluir pessoa jurídica");
         type = sc.nextLine();
-        if (!type.equals("F")) {
+        if (!type.equalsIgnoreCase("F")) {
             System.out.println("Digite o id da pessoa que você quer excluir.");
             int pessoaId = sc.nextInt();
             PessoaFisicaRepo.excluir(pessoaId);
             System.out.println("Pessoa excluida com sucesso");
-        } else if(!type.equals("J")) {
+        } else if(!type.equalsIgnoreCase("J")) {
             System.out.println("Digite o id da pessoa que você quer excluir.");
             int pessoaId = sc.nextInt();
             System.out.println("Pessoa excluida com sucesso");
@@ -43,12 +48,12 @@ public class Menu {
     public static void buscarPeloId() {
         System.out.println("F - Buscar pessoa física | J - Buscar pessoa jurídica");
         type = sc.nextLine();
-        if (!type.equals("F")) {
+        if (!type.equalsIgnoreCase("F")) {
             System.out.println("Digite o id da pessoa que você quer excluir.");
             int pessoaId = sc.nextInt();
             PessoaFisicaRepo.obter(pessoaId);
 
-        } else if(!type.equals("J")) {
+        } else if(!type.equalsIgnoreCase("J")) {
             System.out.println("Digite o id da pessoa que você quer excluir.");
             int pessoaId = sc.nextInt();
 
